1593dadee84b512bee35046fa164e5b0613b4530
Daniel Marzari <Dan.marzari@gmail.com>
2018-08-10 10:36:14 -0400

    Update fourierSettings.java
    Added user variables for audio signal

--- fourierSettings.java
+++ fourierSettings.java
@@ -1,26 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fourier_transform;
 
 /**
  *
- * @author Daniel
+ * @author Daniel Marzari
  */
 public class fourierSettings {
-    static int[] frequencies = new int[] {1,27};
-    static double[] amplitudes = new double[] {2,1};
-    static double[] phase = new double[] {0,1};
+    static int[] frequencies = new int[] {440,490, 550};
+    static double[] amplitudes = new double[] {1,1,1};
+    static double[] phases = new double[] {0,1,1};
     
-    public int max_detectedFrequency = 30; //max frequency the algorithm will search for
-    public int min_detectedFrequency = 0; //min frequency the algorithm will search for
+    public int max_detectedFrequency = 600; //max frequency the algorithm will search for
+    public int min_detectedFrequency = 400; //min frequency the algorithm will search for
     
     public int detectedFrequencyRange = max_detectedFrequency - min_detectedFrequency;
      
     public int sampleRate = (max_detectedFrequency * 2);
+    public int audioRate = 44100;
     public int graphRateMultiplyer = 10;
+    public int audioDuration = 5;  // in seconds
     
     int frequencyCount = frequencies.length;
     
